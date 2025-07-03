1bb8331e71150493835d1224ae30ec647208e16f
abhaytiwaryi <125488267+abhaytiwaryi@users.noreply.github.com>
2025-07-03 11:40:39 +0000

    Conversion of data types

--- JavaScript/basics.js
+++ JavaScript/basics.js
@@ -1,1 +1,15 @@
-console.log("Azure")
+const accountId = 123456
+let accountEmail = "example@gmail.com"
+var accountPassword = "123456"
+accountCity = "Nalanda"
+let accountState;
+
+accountEmail = "exampleexample@gmail.com"
+accountPassword = "654321"
+accountCity = "Patna"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
