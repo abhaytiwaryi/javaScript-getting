09a7a60c9712997e1c492a7a13b7a7e356b84b1b
abhaytiwaryi <125488267+abhaytiwaryi@users.noreply.github.com>
2025-07-04 15:43:59 +0000

    stankAndHeap

--- JavaScript/Stack_and_Heap.js
+++ JavaScript/Stack_and_Heap.js
@@ -1,7 +1,19 @@
-let myName = "Abhay"
-let firstName = myName
-firstName = "Tiwary"
+// let myName = "Abhay"
+// let firstName = myName
+// firstName = "Tiwary"
 
 
-console.log(myName);
-console.log(firstName);
+// console.log(myName);
+
+let userOne = {
+    email: "example@gmail.com",
+    phone: 124456
+}
+
+let userTwo = userOne
+
+userTwo.email = "abhay@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
+
